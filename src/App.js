80724babac3c09f2290641f0660e3d875d79f03a
Rafiya Mateen <r.mateen666@gmail.com>
2020-06-04 08:27:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World from Rafiya Mateen</h1>
+      <h1>Hello World From Rafiya Mateen</h1>
     </div>
   );
 }
